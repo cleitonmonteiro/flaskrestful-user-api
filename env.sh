07c14101bfbf3614b4c549bc7ed1030d785ab048
newtonius <mateusfrancofs@hotmail.com>
2019-05-01 12:03:18 -0300

    env desnecessario

--- env.sh
+++
@@ -1,7 +0,0 @@
-export FLASK_APP=run.py
-export FLASK_CONFIG=development
-# export SECRET_KEY=
-# export DEV_DATABASE_URL=
-# export TEST_DATABASE_URL=
-# export DATABASE_URL= >> Configurar mysql ou postgress
-
